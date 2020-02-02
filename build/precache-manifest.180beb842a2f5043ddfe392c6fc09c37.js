ac78ae5c898f14cbe680355ee1a7b6fef68f3e6a
stank2010 <stankkpp_hualaem@hotmail.com>
2020-02-02 16:19:10 +0700

    can use project-2

---
+++ build/precache-manifest.180beb842a2f5043ddfe392c6fc09c37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e2d2d8fef9b42f36db4d93aea0b093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01d91e5e524c0ecd891b",
+    "url": "/static/css/2.c626c539.chunk.css"
+  },
+  {
+    "revision": "e473f0f4371e95b19b66",
+    "url": "/static/css/main.41330adb.chunk.css"
+  },
+  {
+    "revision": "01d91e5e524c0ecd891b",
+    "url": "/static/js/2.a5485569.chunk.js"
+  },
+  {
+    "revision": "8571f58cc1334518d6fa98782f65842c",
+    "url": "/static/js/2.a5485569.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e473f0f4371e95b19b66",
+    "url": "/static/js/main.9a96699f.chunk.js"
+  },
+  {
+    "revision": "755e79db65c34156829f",
+    "url": "/static/js/runtime-main.2ea539ab.js"
+  },
+  {
+    "revision": "d43261b18052b2cdf44c6778e4205f17",
+    "url": "/static/media/Capture.d43261b1.png"
+  }
+]);
